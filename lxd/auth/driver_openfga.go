lxc/incus

incusd/auth/openfga: Fix diff logic to compare the correct objects
Closes #269

Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

internal/server/auth/driver_openfga.go
@@ -906,7 +906,7 @@ func (f *fga) syncResources(ctx context.Context, resources Resources) error {
 				return err
 			}
 
-			if !util.ValueInSlice(remoteObject, resources.CertificateObjects) {
+			if !util.ValueInSlice(remoteObject, localObjects) {
 				if relation == relationProject {
 					user = ObjectProject(remoteObject.Project()).String()
 				}

markylaing/lxd

Signed-off-by: Mark Laing <mark.laing@canonical.com>

lxd/auth/driver_openfga.go
@@ -940,7 +940,7 @@ func (f *fga) syncResources(ctx context.Context, resources Resources) error {
 				return err
 			}
 
-			if !shared.ValueInSlice(remoteObject, resources.CertificateObjects) {
+			if !shared.ValueInSlice(remoteObject, localObjects) {
 				if relation == relationProject {
 					user = ObjectProject(remoteObject.Project()).String()
 				}