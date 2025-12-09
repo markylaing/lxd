21f0a9f1fd9bd6be352b4921366916e92e846f39
Mark Laing <mark.laing@canonical.com>
2025-12-09 19:15:42 +0000

    lxd: Update calls to Operation.Cancel.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/tokens.go
+++ lxd/tokens.go
@@ -60,10 +60,8 @@ func autoRemoveExpiredTokens(ctx context.Context, s *state.State) {
 
 	opRun := func(ctx context.Context, op *operations.Operation) error {
 		for _, op := range expiredTokenOps {
-			_, err := op.Cancel()
-			if err != nil {
-				logger.Warn("Failed removing expired token", logger.Ctx{"err": err, "operation": op.ID()})
-			}
+			op.Cancel()
+			_ = op.Wait(ctx)
 		}
 
 		err := s.DB.Cluster.Transaction(ctx, func(ctx context.Context, tx *db.ClusterTx) error {
