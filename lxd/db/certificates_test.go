1a72a5efca009694628b91d885cc2d1c6156ea45
Mark Laing <mark.laing@canonical.com>
2026-03-18 15:15:12 +0000

    lxd: Improve certificates test.
    This was failing because the input certificate was empty.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/certificates_test.go
+++ lxd/db/certificates_test.go
@@ -4,6 +4,8 @@ package db_test
 
 import (
 	"context"
+	"encoding/pem"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,20 +14,38 @@ import (
 	"github.com/canonical/lxd/lxd/certificate"
 	"github.com/canonical/lxd/lxd/db"
 	"github.com/canonical/lxd/lxd/db/cluster"
+	"github.com/canonical/lxd/shared"
+	"github.com/canonical/lxd/shared/api"
 )
 
 func TestGetCertificate(t *testing.T) {
 	tx, cleanup := db.NewTestClusterTx(t)
 	defer cleanup()
 
+	testCertPair := shared.TestingKeyPair()
+	testCert, err := testCertPair.PublicKeyX509()
+	require.NoError(t, err)
+	testCertString := string(pem.EncodeToMemory(&pem.Block{Bytes: testCert.Raw, Type: "CERTIFICATE"}))
+	fingerprint := shared.CertFingerprint(testCert)
+
 	ctx := context.Background()
-	_, err := cluster.CreateCertificateLegacy(ctx, tx.Tx(), cluster.CertificateLegacy{
-		Fingerprint: "foobar",
+	_, err = cluster.CreateCertificateLegacy(ctx, tx.Tx(), cluster.CertificateLegacy{
+		Name:        "test-cert",
+		Fingerprint: fingerprint,
+		Certificate: testCertString,
 		Type:        certificate.TypeClient,
 	})
 	require.NoError(t, err)
 
-	cert, err := cluster.GetCertificateLegacy(ctx, tx.Tx(), "foobar")
+	cert, err := cluster.GetCertificateLegacy(ctx, tx.Tx(), "incorrect")
+	require.Error(t, err)
+	require.True(t, api.StatusErrorCheck(err, http.StatusNotFound))
+	require.Nil(t, cert)
+
+	cert, err = cluster.GetCertificateLegacy(ctx, tx.Tx(), fingerprint)
 	require.NoError(t, err)
-	assert.Equal(t, "foobar", cert.Fingerprint)
+	assert.Equal(t, fingerprint, cert.Fingerprint)
+	assert.Equal(t, "test-cert", cert.Name)
+	assert.Equal(t, testCertString, cert.Certificate)
+	assert.Equal(t, certificate.TypeClient, cert.Type)
 }
